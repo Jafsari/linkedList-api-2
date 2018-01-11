65ea1e57f3b714bd9252256fefda1a49c95722de
Zejian Shen <zesansan@gmail.com>
2018-01-10 17:15:41 -0800

    allowing CORS

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 // npm packages
-const bodyParser = require('body-parser');
-const express = require('express');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
+const bodyParser = require("body-parser");
+const express = require("express");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
 
 // app imports
 
@@ -10,31 +10,43 @@ const morgan = require('morgan');
 const app = express();
 
 // app config
-app.use(bodyParser.json({ type: '*/*' }));
-// we are using the express.static middleware and specifying a path for 
+app.use(bodyParser.json({ type: "*/*" }));
+// we are using the express.static middleware and specifying a path for
 //static files to be found. __dirname is a variable that we can use to refer
 // to the directory name of the current module)
-app.use(express.static(__dirname + '/public')); 
-app.use(methodOverride('_method')); // ?_method=PATCH
-
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method")); // ?_method=PATCH
 
 // app imports
-const { usersRouter } = require('./routes');
-const { companiesRouter } = require('./routes');
-const { jobsRouter } = require('./routes');
+const { usersRouter } = require("./routes");
+const { companiesRouter } = require("./routes");
+const { jobsRouter } = require("./routes");
 
-
-app.get('/', (request, response, next) => { //??????how to get to companies
-  return response.redirect('/users');
+app.get("/", (request, response, next) => {
+	//??????how to get to companies
+	return response.redirect("/users");
 });
 
-app.use('/users', usersRouter);
-app.use('/companies', companiesRouter);
-app.use('/jobs', jobsRouter);
+app.use("/users", usersRouter);
+app.use("/companies", companiesRouter);
+app.use("/jobs", jobsRouter);
 //app.use('/companies/:company_id/jobs', jobsRouter);
 
-
-app.listen(3000, () => {
-  console.log('Express Templating Server listening on port 3000');
+app.listen(3001, () => {
+	console.log("Express Templating Server listening on port 1");
 });
 
+//allow CORS
+app.use((request, response, next) => {
+	response.header("Access-Control-Allow-Origin", "*");
+	response.header(
+		"Access-Control-Allow-Headers",
+		"Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+	);
+	response.header(
+		"Access-Control-Allow-Methods",
+		"POST,GET,PATCH,DELETE,OPTIONS"
+	);
+	response.header("Content-Type", "application/json");
+	next();
+});
